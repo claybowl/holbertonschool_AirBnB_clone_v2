3869fb773d8b9e6c6730050ca13d556f80ae211a
claybowl <dopeanddaytrading@gmail.com>
2023-03-04 17:18:20 -0600

    mysql file creation

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Script to create new Database
+-- Creates user hbnb_test with all privileges
+CREATE DATABASE
+IF NOT EXISTS hbnb_test_db;
+CREATE USER
+IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
