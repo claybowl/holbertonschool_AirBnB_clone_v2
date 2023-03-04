7943708b71780fd5515f0204d3128ed09da74c1b
claybowl <dopeanddaytrading@gmail.com>
2023-03-04 16:46:13 -0600

    city attempt

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ class City, containing state ID, place, and name """
+
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
